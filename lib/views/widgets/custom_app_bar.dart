14c85a64836d69687033e1451ac76c5e1b4c38db
mohamedsaeedofficial <mohameed22112001@gmail.com>
2024-08-03 16:27:16 +0300

    create notes view body with customise appBar

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon()
+      ],
+    );
+  }
+}
