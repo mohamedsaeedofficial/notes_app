708c293dfb8fc78640e875be7bdfdeea18f7bf3c
mohamedsaeedofficial <mohameed22112001@gmail.com>
2024-08-03 20:16:54 +0300

    add Note buttom show

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -10,7 +11,11 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       body: const NotesViewBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(context: context, builder: (context) {
+            return const AddNoteBottomSheet() ;
+          },);
+        },
         backgroundColor: const Color.fromARGB(255, 17, 212, 238),
         shape: const CircleBorder(
           side: BorderSide(
