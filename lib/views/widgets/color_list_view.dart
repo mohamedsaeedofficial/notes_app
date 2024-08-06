f199d5327fde62383cb86e7b242e5c1d2cf0b121
mohamedsaeedofficial <mohameed22112001@gmail.com>
2024-08-06 17:22:06 +0300

    create a color list for notes

--- lib/views/widgets/color_list_view.dart
+++ lib/views/widgets/color_list_view.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/colors_view.dart';
 
-class ColorListView extends StatelessWidget {
+class ColorListView extends StatefulWidget {
   const ColorListView({super.key});
 
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xffE4FDE1) ,
+    Color(0xff8acb88) ,
+    Color(0xff648381) ,
+    Color(0xff575761) ,
+    Color(0xff068d9d) ,
+    Color(0xff53599a) ,
+    Color(0xff6d9dc5) ,
+    Color(0xff80ded9) ,
+    Color(0xffaeecef) ,
+    Color(0xffffbf46) ,
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 60,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 5,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-          return const ColorsView();
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorsView(
+                color: colors[index] ,
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
         },
       ),
     );
--- lib/views/widgets/colors_view.dart
+++ lib/views/widgets/colors_view.dart
@@ -1,14 +1,23 @@
-
 import 'package:flutter/material.dart';
 
 class ColorsView extends StatelessWidget {
-  const ColorsView({super.key});
-
+  const ColorsView({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final Color color ;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 30,
-      backgroundColor: Colors.blue ,
-    );
+    return isActive
+        ?  CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 26,
+              backgroundColor: color,
+            ),
+          )
+        :  CircleAvatar(
+            radius: 28,
+            backgroundColor: color,
+          );
   }
 }
